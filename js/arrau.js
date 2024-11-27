1d7d66dfcc49376cc8cbea72d38de51eda3b766d
Ailebiz <bolkyzyaliza@gmail.com>
2024-11-27 16:10:20 +0500

    no

---
+++ js/arrau.js
@@ -0,0 +1,16 @@
+let futbolshy = ["Роналду" , "Зидан", "Неймар", "Бензема"]
+
+futnolshy[1] = "Месси"
+futbolshy[4] = "Суарес"
+futbolshy[5] = "Беллингем"
+futbolshy[6] = "Мбаппе"
+futbolshy[7] = "Салах"
+
+console.log(futbolshy);
+
+
+if(futbolshy.length > 5){
+    console.log("more");
+}else{
+    console.log("A little");
+}
